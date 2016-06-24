d24439ff2fb23c32b09bb29faa1789a10a64a2f8
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-24 13:16:44 +0100

    8.2 Modified program to only print the students whose name begins with a specific letter

--- directory.rb
+++ directory.rb
@@ -4,26 +4,30 @@ def input_students
   # creating an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
   # while the name is not empty. repeat this code
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # return the array of students
   students
 end
 
 def print_header
-puts "The students of Villains Academy"
-puts "-------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
+
 def print(students)
+  puts "Starting with which letter would you like to see student names?"
+  letter = gets.chomp.capitalize
   students.each_with_index do |student, index|
-    number = (index + 1).to_s
-  puts "#{number}.#{student[:name]} (#{student[:cohort]} cohort)"
+    if letter == student[:name][0]
+      puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
