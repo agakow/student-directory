6ec979aad4bed01cc7413b35d8ecb0d16931bb26
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-27 21:06:53 +0100

    11 Saving data to file

--- directory.rb
+++ directory.rb
@@ -9,6 +9,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -24,6 +25,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -111,5 +114,14 @@ def print_footer
   puts
 end
 
-#print_cohort(students)
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:dob], student[:country_ob], student[:hobby]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 interactive_menu
