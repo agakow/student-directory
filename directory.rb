157a6fb205b1d5c07a80bc79c3826968d3e443d6
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-27 12:45:26 +0100

    8.7 Adding cohort input

--- directory.rb
+++ directory.rb
@@ -1,13 +1,22 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return."
-  # creating an empty array
   students = []
-  # get the first name
   name = gets.chomp.capitalize
   # while the name is not empty. repeat this code
   while !name.empty? do
-    puts "Please enter date of birth for #{name} (dd/mm/yyyy)"
+    puts "Please enter cohort month"
+    cohort = gets.chomp.capitalize
+    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      while !month.include?(cohort) || cohort.empty? do
+        if cohort.empty?
+          cohort = month[Time.now.month - 1]
+        elsif !month.include?(cohort)
+         puts "Please enter a valid month or hit return for default (current month)."
+         cohort = gets.chomp.capitalize
+        end
+      end
+    puts "Please enter date of birth for #{name} (dd-mm-yyyy)"
     dob = gets.chomp
     puts "Please enter the country of birth for #{name}."
     puts "If unknown, hit return."
@@ -16,8 +25,12 @@ def input_students
         country_ob = "unknown"
       end
     puts "Please enter #{name}'s hobby"
+    puts "If unknown, hit return."
     hobby = gets.chomp.capitalize
-    students << {name: name, cohort: :november, dob: dob, country_ob: country_ob, hobby: hobby}
+      while hobby.empty?
+      hobby = "unknown"
+      end
+    students << {name: name, cohort: cohort, dob: dob, country_ob: country_ob, hobby: hobby}
     puts "Now we have #{students.count} students."
     puts "Please enter the name of another student."
     puts "To finish hit return."
